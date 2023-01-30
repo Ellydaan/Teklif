7660f56f0e178b8864a7219914f2052008928e8e
ellidaan <elone.meimoun@epitech.digital>
2023-01-30 18:20:53 +0100

    logo /

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img class="logo" src="../assets/logo_tek.png" alt="logo_tek"></a>
+    <router-link to="/" class="navbar-brand" href="#"><img class="logo" src="../assets/logo_tek.png" alt="logo_tek"> </router-link>
+    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
