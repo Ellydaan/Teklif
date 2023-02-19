371aef2089ba34ead75ea9ae98c24d1d96a1f31f
ellidaan <elone.meimoun@epitech.digital>
2023-02-19 14:03:10 +0100

    Front

--- src/components/HOME/Domain.vue
+++ src/components/HOME/Domain.vue
@@ -1,63 +1,33 @@
 <template>
-<div class="ctn">
-  <div class="item1">
+  <div class="CTN">
+    <div class="ctn">
+      <div class="ctnitem">
 
-  <h1>DES ÉTUDIANTS QUALIFIÉS DANS DIVERS DOMAINES </h1>
-  </div>
+    <div class="item1">
+      <h1><span>Trouvez l’étudiant</span> qu’il vous faut</h1>
+      <p>Teklif propose une liste d’étudiants issus d’Universités ou de Grandes Ecoles et certifient leur aptitude à réaliser vos missions. Grâce à leur énergie et à leur maîtrise de la théorie, ils sauront apporter nouveauté et rigueur aux entreprises. Teklif offre ainsi l’accès à un large panel d’étudiants, qui pourront devenir de précieux collaborateurs.
+      </p>
 
-  <div class="item2">
+    </div>
+    <div class="item2"><img src="../../assets/etu.png" alt="etud"></div>
+      </div>
+      <div class="ctnitem">
 
-    <div class="card" >
-      <img src="../../assets/img2.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">DATA & TECHNOLOGIE</p>
+        <div class="item2">
+          <img src="../../assets/coupe.png" alt="couple">
+
+        </div>
+        <div class="item1">
+          <h1><span>Mettez vos enseignements</span> en pratique</h1>
+          <p> Grâce à Teklif, réalisez des missions qui vous permettront de gagner en expérience tout en étant rémunéré. Les missions sont l’opportunité parfaite de se faire connaître auprès d’un futur employeur.
+          </p>
+
+        </div>
       </div>
     </div>
-    <div class="card" >
-      <img src="../../assets/img1.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">MARKETING & COMMUNICATION</p>
-      </div>
-    </div>
-    <div class="card">
-      <img src="../../assets/img3.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">SCIENCES & INGÉNIERIE</p>
-      </div>
-    </div>
-
-
-
-  </div>
-
-  <div class="item2">
-    <div class="card" >
-      <img src="../../assets/img4.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">ÉCONOMIE & GESTION</p>
-      </div>
-    </div>
-    <div class="card" >
-      <img src="../../assets/img5.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">DESIGN & CRÉATION</p>
-      </div>
-    </div>
-    <div class="card">
-      <img src="../../assets/img6.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">SANTÉ &  BIOLOGIE</p>
-      </div>
-    </div>
-
-
 
   </div>
 
-
-
-
-  </div>
 
 
 </template>
@@ -69,74 +39,67 @@ export default {
 </script>
 
 <style scoped>
-.ctn{
+.CTN{
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
+ flex-direction: row;
+  padding: 50px 0;
   width: 100%;
   height: 100%;
-  padding-top: 100px ;
 }
-.item2{
+.item1{
   display: flex;
-  justify-content: space-evenly;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
+  align-self: center;
+  width: 50%;
+  height: 100%;
+
+
+}
+.ctnitem{
+  display: flex;
   flex-direction: row;
   width: 100%;
   height: 100%;
-  margin-top: 30px;
-
 }
-.card{
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  border: black solid 1px;
-  width: 20%;
-
-}
-.card-text{
-  text-align: center;
-
-}
-.card-body{
-  background-color: #D9D9D9;
-}
-h1{
-  margin-bottom: 90px;
-
-}
-@media screen and (max-width: 1300px) {
 .ctn{
+  display: flex;
+ flex-direction: column;
+  width: 100%;
   height: 100%;
-  flex-wrap: wrap;
+
+}
+
+.item2{
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0;
+  width: 50%;
+  height: 100%;
+
 
 }
-  .item2{
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-  margin-top: 30px;
-  }
-  .card{
-    width: 80%;
-    margin-bottom: 30px;
-  }
-  h1{
-    text-align: center;
-    font-size: 30px;
-    height: 100%;
-    margin-bottom: 20px;
+p{
 
+  font-size: 20px;
+  color: black;
+  font-weight: 500;
+  width: 80%;
+  height: 50%;
+}
+h1{
 
-  }
+  width: 80%;
 
+  color: #000000;
+  font-size: 40px;
+  font-weight: 600;
+  margin: 0;
+  padding-bottom: 30px;
+}
+span{
+  color: #C03AFE;
 }
 
 
--- src/components/HOME/Partenaire.vue
+++ src/components/HOME/Partenaire.vue
@@ -1,16 +1,48 @@
 <template>
-<div class="ctn_flex">
-  <div class="item_txt">
+  <div class="CTN">
 
-<h1>DES PARTENAIRES INTERNATIONAUX</h1>
-    <p>Des entreprises et institutions à la renomée internationale croient en Teklif. Ils proposent des missions aux étudiants, à la recherche de leur futurs collaborateurs.</p>
-      <ButtonS/>
+    <div class="card">
+      <div class="titre">
+        <h1><span>Domaines d’expertise  </span>des étudiants</h1>
+      </div>
+
+      <div class="CTNcard">
+        <div class="Cardd">
+          <img src="../../assets/roue.png"  alt="roue">
+          <p>Sciences
+            Ingénierie</p>
+
+        </div>
+        <div class="Cardd">
+          <img src="../../assets/phone.png" alt="phone">
+          <p>Marketing
+            Communication</p>
+        </div>
+        <div class="Cardd">
+          <img src="../../assets/dev.png" alt="ordinateur">
+          <p>Informatique</p>
+        </div>
+        <div class="Cardd">
+          <img src="../../assets/art.png" alt="art">
+          <p>Design
+            Création</p>
+        </div>
+        <div class="Cardd">
+          <img src="../../assets/arg.png" alt="arg">
+          <p>Économie
+            Gestion</p>
+        </div>
+        <div class="Cardd">
+          <img src="../../assets/adn.png" alt="adn">
+          <p>Santé
+            Biologie</p>
+        </div>
+      </div>
 
 
-</div>
+    </div>
 
   </div>
-
 
 
 </template>
@@ -27,62 +59,67 @@ export default {
 
 <style scoped>
 
-.ctn_flex{
-  margin-top: 90px;
-  color: white;
-  width: 100%;
-  height: 100%;
+.CTN {
   display: flex;
   justify-content: center;
   align-items: center;
-
-
-
-}
-.item_txt{
-  text-align: center;
+  height: 100%;
   width: 100%;
-  height: 50%;
-  background-color: rgb(233, 78, 27,0.5);
+  flex-wrap: wrap;
+  margin-bottom: 40px;
+  margin-top: 5%;
+}
+h1{
+  color: black;
+  font-size: 40px;
+  font-weight: 600;
+  margin: 0;
+  text-align: center;
+}
+
+.card{
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  padding: 20px;
-
-  color: #000000;
+  width: 80%;
+  height: 90%;
+  border: none;
 
 }
+.CTNcard{
+  display: flex;
+  justify-content: space-around;
+align-items: flex-start;
+  flex-wrap: wrap;
+  width: 65%;
+  height: 100%;
+  padding-top: 70px;
+  gap: 25px;
+}
+.Cardd{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 
+  width: 227px ;
+  height: 252px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+
+
+}
 p{
-  margin-bottom: 20px;
-  width: 40%;
   font-size: 20px;
-
+  font-weight: 600;
+  color: black;
+  text-align: center;
 }
-@media screen and (max-width: 1300px) {
-  .item_txt{
-    width: 100%;
-    height: 50%;
-    background-color: rgb(233, 78, 27,0.5);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 60px;
-  }
-  h1{
-    font-size: 30px;
-  }
-  p{
-    font-size: 15px;
-    width: 80%;
-  }
-
-
-
+span{
+  color: #5B03E4;
 }
-
 
 
 
